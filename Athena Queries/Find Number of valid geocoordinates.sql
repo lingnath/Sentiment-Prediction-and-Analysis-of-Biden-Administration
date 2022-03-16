70b4a1abea9a7732ee4c9a8d85b26f400a05b350
lingnath <32849838+lingnath@users.noreply.github.com>
2022-03-15 17:00:45 -0700

    Delete Athena Queries directory

--- Athena Queries/Find Number of valid geocoordinates.sql
+++
@@ -1,5 +0,0 @@
--- Find the number of rows where geocoordinates exist for the tweet
-SELECT count(*)-
-       (SELECT count(*) FROM twitter_analysis.twitter_raw
-        where geocoordinates='None' or geocoordinates is NULL)
-FROM twitter_analysis.twitter_raw;
